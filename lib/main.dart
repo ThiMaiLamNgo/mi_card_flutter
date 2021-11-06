931e30ffdd2a43474035da15f4e0e5efda891583
Thi Mai Lam Ngo <thimailamngo@gmail.com>
2021-11-06 17:03:51 +0000

    Layout challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,93 @@
 import 'package:flutter/material.dart';
+//import 'package:mi_card/main.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  //const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundColor: Colors.red,
+            ),
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
+/*
+//LAYOUT CHALLENGE:
+import 'package:flutter/material.dart';
+//import 'package:mi_card/main.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  //const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            children: <Widget>[
+              Container(
+                height: 600.0,
+                width: 100.0,
+                color: Colors.red,
+                //child: Text('Container 1'),
+              ),
+              SizedBox(
+                width: 55.0,
+              ),
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      color: Colors.yellow,
+                      //child: Text('Container 21'),
+                    ),
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      color: Colors.green,
+                      //child: Text('Container 22'),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: 55.0,
+              ),
+              Container(
+                height: 600.0,
+                width: 100.0,
+                color: Colors.blue,
+                //child: Text('Container 3'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+ */
