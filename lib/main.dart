71b1cc5e55885f48335631adc9ae790a3963caf5
Thi Mai Lam Ngo <thimailamngo@gmail.com>
2021-11-06 21:33:29 +0000

    micard is ready

--- lib/main.dart
+++ lib/main.dart
@@ -15,18 +15,113 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
             child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             CircleAvatar(
-              radius: 50.0,
-              backgroundColor: Colors.red,
+              radius: 100.0,
+              backgroundImage: AssetImage('images/TMLN.PNG'),
             ),
+            Text(
+              'Thi Mai Lam Ngo',
+              style: TextStyle(
+                fontFamily: 'Tajawal',
+                fontSize: 40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Student',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                color: Colors.teal.shade100,
+                fontSize: 25.0,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.white,
+              ), //Divider
+            ), //SizedBox
+            //with padding
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.5),
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone_iphone,
+                      size: 40.0,
+                      color: Colors.black,
+                    ),
+                    SizedBox(
+                      width: 15.0,
+                    ),
+                    Text(
+                      '+354567891',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            //Card with ListTile:
+            Card(
+                //padding: EdgeInsets.all(10.0), //Cards don´t have padding
+                //color: Colors.white, //because Cards by default come with white color.
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.5),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    size: 40.0,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    'tmln1@nemandi.ntv.is',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                )),
+            //Card with ListTile:
+            Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.5),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.video_library,
+                    size: 40.0,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    'Wonderful Life',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                )),
           ],
         )),
       ),
     );
   }
 }
+
 /*
+
 //LAYOUT CHALLENGE:
 import 'package:flutter/material.dart';
 //import 'package:mi_card/main.dart';
@@ -90,4 +185,31 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+//CONTAINER WIDGET:
+Container(
+              padding: EdgeInsets.all(10.0),
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.5),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.video_library,
+                    size: 40.0,
+                    color: Colors.black,
+                  ),
+                  SizedBox(
+                    width: 15.0,
+                  ),
+                  Text(
+                    'Wonderful Life',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
  */
